94817866a4dd42930595f82a12503dc013f6e658
BrianPhipps <phipps@lovetoknow.com>
2020-05-30 00:55:22 -0700

    add router, add facicon, add icons to menu, and home page

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,138 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
+  <v-app id="inspire">
+    <v-navigation-drawer
+            v-model="drawer"
+            app
+            clipped
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-compass-rose</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>      <router-link to="/">Map</router-link> </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-account-group</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>      <router-link to="/">Characters</router-link> </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-timeline-text</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>      <router-link to="/">Timeline</router-link> </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+<!--            <v-icon>mdi-bottle-tonic</v-icon>-->
+          <v-icon>mdi-sword</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>      <router-link to="/">Equipment</router-link> </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+    <v-app-bar
+            app
+            clipped-left
 
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-spacer></v-spacer>
+      <v-toolbar-title>Great Advanture</v-toolbar-title>
+        <v-spacer></v-spacer>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <router-view></router-view>
+<!--      <v-container-->
+<!--              class="fill-height"-->
+<!--              fluid-->
+<!--      >-->
+<!--        <v-row-->
+<!--                align="center"-->
+<!--                justify="center"-->
+<!--        >-->
+<!--          <v-col class="shrink">-->
+<!--            <v-tooltip right>-->
+<!--              <template v-slot:activator="{ on }">-->
+<!--                <v-btn-->
+<!--                        :href="source"-->
+<!--                        icon-->
+<!--                        large-->
+<!--                        target="_blank"-->
+<!--                        v-on="on"-->
+<!--                >-->
+<!--                  <v-icon large>mdi-code-tags</v-icon>-->
+<!--                </v-btn>-->
+<!--              </template>-->
+<!--              <span>Source</span>-->
+<!--            </v-tooltip>-->
+<!--          </v-col>-->
+<!--        </v-row>-->
+<!--      </v-container>-->
     </v-content>
+
+<!--    <v-footer app>-->
+<!--      <span>&copy; 2020</span>-->
+<!--    </v-footer>-->
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
-export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+  export default {
+    props: {
+      source: String,
+    },
+    data: () => ({
+      drawer: null,
+    }),
+    created () {
+      this.$vuetify.theme.dark = true
+    },
+  }
 </script>
+
+
+<!--<template>-->
+<!--  <div id="app">-->
+<!--    <div id="nav">-->
+<!--      <router-link to="/">Home</router-link> |-->
+<!--      <router-link to="/about">About</router-link>-->
+<!--    </div>-->
+<!--    <router-view/>-->
+<!--  </div>-->
+<!--</template>-->
+
+<!--<style lang="scss">-->
+<!--#app {-->
+<!--  font-family: Avenir, Helvetica, Arial, sans-serif;-->
+<!--  -webkit-font-smoothing: antialiased;-->
+<!--  -moz-osx-font-smoothing: grayscale;-->
+<!--  text-align: center;-->
+<!--  color: #2c3e50;-->
+<!--}-->
+
+<!--#nav {-->
+<!--  padding: 30px;-->
+
+<!--  a {-->
+<!--    font-weight: bold;-->
+<!--    color: #2c3e50;-->
+
+<!--    &.router-link-exact-active {-->
+<!--      color: #42b983;-->
+<!--    }-->
+<!--  }-->
+<!--}-->
+<!--</style>-->
---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-app id="inspire">
+
+<!--     <h1>Exploration, Social Interaction, and Combat</h1>-->
+
+                <v-img class="enableBlur" src="../assets/HarlanMap.png" aspect-ratio="1.7">
+                    <v-row align="center" class="lightbox white--text pa-2 fill-height">
+
+                        <v-col class="mt-n17" style="text-align: center; height: 200px; padding: 40px; background-color: rgba(0,0,100,.15); box-shadow: 2px 2px 8px 8px rgba(0,50,0,.15) " >
+                            <div class="display-3 font-weight-bold">Exploration, Social Interaction, and Combat</div>
+                        </v-col>
+
+                    </v-row>
+                </v-img>
+
+
+  </v-app>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
